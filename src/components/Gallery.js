b48364ccec66bad438e2c8b83ae1fbd75b55857d
Ankit Jain <ankit.jain67@t-mobile.com>
2021-02-20 02:04:32 -0800

    reverting the change

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,7 +7,7 @@ function Gallery() {
     const inputElement = useRef(null);
     const [images, setImages] = useState([]);
     const [showLoader, setShowLoader] = useState(false);
-    let [enlargeImage, setEnlargeImage] = useState(false);
+    const [enlargeImage, setEnlargeImage] = useState(false);
     let [spinnerColor, setSpinnerColor] = useState("#f7b244");
     const [error, setError] = useState('');
 
@@ -17,7 +17,7 @@ function Gallery() {
     margin: 0 auto;
     `;
 
-    function handleClick(e) {
+    function handleClick(e){
         e.preventDefault();
         setShowLoader(true);
         const query = handleOnchange();
@@ -34,9 +34,9 @@ function Gallery() {
                 setShowLoader(false);
             })
         })
-            .catch(error => {
-                setError(error);
-            })
+        .catch(error => {
+            setError(error);
+        })
     }
 
     function handleOnchange() {
@@ -49,7 +49,7 @@ function Gallery() {
     }
 
     function handleEnlargeImage(image) {
-        setEnlargeImage(!enlargeImage[image]);
+        console.log(image.id);
     }
 
     return (
@@ -69,43 +69,20 @@ function Gallery() {
                 <div className="row d-flex">
                     {
                         showLoader ? <ClipLoader size={100} color={spinnerColor} css={override} /> :
-                            images && images.map(image =>
-                                <div className="col-lg-4 col-md-4 col-sm-4 mt-5" key={image.id}>
-                                    <div className="card mb-4" >
-                                        {
-                                            !enlargeImage ?
-                                                image && image.images && image.images[0] ?
-                                                    <img className="card-img-top m-auto p-1" src={image.images[0].link} onError={handleImgError} onClick={handleEnlargeImage.bind(null, image)} /> : <img className="card-img-top m-auto p-1" src="https://picsum.photos/200/300" onError={handleImgError} onClick={handleEnlargeImage.bind(null, image)} />
-                                                :
-                                                //         <dialog className="enlarged"
-                                                //             style={{ position: 'absolute' }}
-                                                //             open onClick={handleEnlargeImage.bind(null, image)}>
-                                                //             image && image.images && image.images[0] ?
-                                                // <img className="card-img-top m-auto p-1" src={image.images[0].link} onError={handleImgError} /> : <img className="card-img-top m-auto p-1" src="https://picsum.photos/200/300" onError={handleImgError} />
-                                                //         </dialog>
-
-                                                <dialog
-                                                    className="dialog"
-                                                    style={{ position: 'absolute' }}
-                                                    open
-                                                    onClick={handleEnlargeImage.bind(null, image)}
-                                                >
-                                                    <img
-                                                        className="image"
-                                                        src="https://picsum.photos/200/300"
-                                                        onClick={handleEnlargeImage.bind(null, image)}
-                                                        alt="no image"
-                                                    />
-                                                </dialog>
-
-                                        }
-
-                                        <div className="card-body">
-                                            <h5 className="card-title">{image.account_url}</h5>
-                                        </div>
+                        images && images.map(image =>
+                            <div className="col-lg-4 col-md-4 col-sm-4 mt-5" key={image.id}>
+                                <div className="card mb-4" >
+                                    {
+                                        image && image.images && image.images[0] ? 
+                                        <img className="card-img-top m-auto p-1" src={image.images[0].link} onError={handleImgError} onClick={handleEnlargeImage.bind(null, image)}/> : <img className="card-img-top m-auto p-1" src="https://picsum.photos/200/300" onError={handleImgError} onClick={handleEnlargeImage.bind(null, image)}/>
+                                    }
+                                    
+                                    <div className="card-body">
+                                        <h5 className="card-title">{image.account_url}</h5>
                                     </div>
                                 </div>
-                            )
+                            </div>
+                        )
                     }
                 </div>
 
