44ad2783e52d25e03eddc6e224404018e85e5010
Ankit Jain <ankit.jain67@t-mobile.com>
2021-02-20 12:15:00 -0800

    removing the console statements

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -17,7 +17,7 @@ function Gallery() {
     margin: 0 auto;
     `;
 
-    function handleClick(e){
+    function handleSearch(e){
         e.preventDefault();
         setShowLoader(true);
         const query = handleOnchange();
@@ -28,10 +28,13 @@ function Gallery() {
         }).then(res => {
             const promise = res.json();
             promise.then(response => {
-                const imageData = response.data;
-                console.log(imageData);
-                setImages(imageData);
-                setShowLoader(false);
+                try {
+                    const imageData = response.data;
+                    setImages(imageData);
+                    setShowLoader(false);
+                } catch(error) {
+                    setError(error);
+                } 
             })
         })
         .catch(error => {
@@ -55,7 +58,7 @@ function Gallery() {
     return (
         <div>
             <div className="container m-auto pt-5">
-                <form className="form-inline my-2 my-lg-0 ml-auto" onSubmit={handleClick}>
+                <form className="form-inline my-2 my-lg-0 ml-auto" onSubmit={handleSearch}>
                     <input className="form-control m-0 w-50"
                         type="search"
                         placeholder="Search"
@@ -69,23 +72,21 @@ function Gallery() {
                 <div className="row d-flex">
                     {
                         showLoader ? <ClipLoader size={100} color={spinnerColor} css={override} /> :
-                        images && images.map(image =>
+                        images ? images.map(image =>
                             <div className="col-lg-4 col-md-4 col-sm-4 mt-5" key={image.id}>
                                 <div className="card mb-4" >
                                     {
                                         image && image.images && image.images[0] ? 
                                         <img className="card-img-top m-auto p-1" src={image.images[0].link} onError={handleImgError} onClick={handleEnlargeImage.bind(null, image)}/> : <img className="card-img-top m-auto p-1" src="https://picsum.photos/200/300" onError={handleImgError} onClick={handleEnlargeImage.bind(null, image)}/>
-                                    }
-                                    
+                                    }                                    
                                     <div className="card-body">
                                         <h5 className="card-title">{image.account_url}</h5>
                                     </div>
                                 </div>
                             </div>
-                        )
+                        ) : <p>{error}</p>
                     }
                 </div>
-
             </div>
         </div>
     )
